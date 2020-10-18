ede6bc0ad1cd91b6c9f5f2d173d46a92484310ea
Cong Meng <samuelmc1015@gmail.com>
2020-10-17 21:15:58 -0400

    Add java version

---
+++ 187. Repeated DNA Sequences/RepeatedDNASequences.java
@@ -0,0 +1,41 @@
+/* 
+Run Envrioment:
+Java JDK 14
+
+Time and Space Complexity:
+Time: O(len(s))
+Space: O(len(s))
+Actual Time: 18 ms
+Space: 47.6 MB
+*/
+
+
+import java.util.*;
+
+public class RepeatedDNASequences {
+    public static List<String> findSequences(String s) {
+        if (s.length() < 10) {
+            return new ArrayList<String>();
+        }
+
+        Set<String> visited = new HashSet<String>();
+        Set<String> result = new HashSet<String>();
+
+        for (int i = 0; i < s.length() - 10 + 1; i++) {
+            String temp = s.substring(i, i + 10);
+            
+            if (visited.contains(temp)){
+                result.add(temp);
+            }
+
+            visited.add(temp);
+        }
+
+        return new ArrayList<String>(result);
+    }
+
+    public static void main(String[] args){
+        System.out.println(RepeatedDNASequences.findSequences("AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT"));
+        System.out.println(RepeatedDNASequences.findSequences("AAAAAAAAAAAAA"));
+    }
+}
