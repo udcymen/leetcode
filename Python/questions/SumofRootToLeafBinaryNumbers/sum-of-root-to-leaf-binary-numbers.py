08d5925e15ae70af2b3d75f276629bd5a4290676
Cong Meng <samuelmc1015@gmail.com>
2020-10-21 20:36:44 -0400

    fix function signature

--- Python/questions/SumofRootToLeafBinaryNumbers/sum-of-root-to-leaf-binary-numbers.py
+++ Python/questions/SumofRootToLeafBinaryNumbers/sum-of-root-to-leaf-binary-numbers.py
@@ -1,5 +1,5 @@
 class TreeNode:
-    def __init__(self, val = 0, left = None, right = None):
+    def __init__(self, val:int = 0, left:'TreeNode' = None, right:'TreeNode' = None):
         self.val = val
         self.left = left
         self.right = right
