94b281f0f91882ae2270f68174d3f5859fd336d7
Cong Meng <samuelmc1015@gmail.com>
2020-10-22 22:01:13 -0400

    Add 144. Binary Tree Preorder Traversal

---
+++ Python/questions/BinaryTreePreorderTraversal/README.md
@@ -0,0 +1,17 @@
+## [144. Binary Tree Preorder Traversal](https://leetcode.com/problems/binary-tree-preorder-traversal/)
+
+### Run Environment
+Python 3.9
+
+### Time and Space Complexity:
+#### Solution 1: Recursion
+Time: O(n)  
+Space: O(1)  
+Actual Time: 32 ms  
+Actual Space: 14.1 MB
+
+#### Solution 2: Non-Recursion
+Time: O(n)  
+Space: O(1)  
+Actual Time: 32 ms  
+Actual Space: 14.1 MB
---
+++ Python/questions/BinaryTreePreorderTraversal/binary-tree-preorder-traversal-solution-2.py
@@ -0,0 +1,36 @@
+class TreeNode:
+    def __init__(self, val:int = 0, left:'TreeNode' = None, right:'TreeNode' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+# Solution 2: Non-Recursion
+def preorderTraversal(root: TreeNode) -> list[int]:
+    result = []
+        
+    if not root:
+        return result
+    
+    stack = [root]
+    
+    while stack:
+        node = stack.pop()
+        
+        while node:
+            result.append(node.val)
+            
+            if node.right:
+                stack.append(node.right)
+                
+            node = node.left
+
+    return result
+
+
+if __name__ == "__main__":
+    root = TreeNode(1)
+    root.right = TreeNode(2)
+    root.right.left = TreeNode(3)
+
+    print(preorderTraversal(root))
