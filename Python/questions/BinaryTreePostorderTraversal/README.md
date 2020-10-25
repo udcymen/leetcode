882a9814edbf8b4f8a17e3dc4989fbacdf41f7b2
Cong Meng <samuelmc1015@gmail.com>
2020-10-25 18:54:44 -0400

    Add 145. Binary Tree Postorder Traversal

---
+++ Python/questions/BinaryTreePostorderTraversal/README.md
@@ -0,0 +1,12 @@
+## [145. Binary Tree Postorder Traversal](https://leetcode.com/problems/binary-tree-postorder-traversal/)
+
+### Run Environment
+Python 3.9
+
+### Time and Space Complexity:
+#### Solution 1: Recursion
+Time: O(n)  
+Space: O(n) - function call stack
+Actual Time: 28 ms  
+Actual Space: 14.1 MB
+
---
+++ Python/questions/BinaryTreePostorderTraversal/binary-tree-postorder-traversal-solution-1.py
@@ -0,0 +1,30 @@
+class TreeNode:
+    def __init__(self, val:int = 0, left:'TreeNode' = None, right:'TreeNode' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+# Solution 1: Recursion
+def traversal(node: TreeNode, result: list[int]):
+    if not node:
+        return
+    
+    traversal(node.left, result)
+    traversal(node.right, result)
+    result.append(node.val)
+
+def postorderTraversal(root: TreeNode) -> list[int]:
+    result = []
+    
+    traversal(root, result)
+
+    return result
+
+
+if __name__ == "__main__":
+    root = TreeNode(1)
+    root.right = TreeNode(2)
+    root.right.left = TreeNode(3)
+
+    print(postorderTraversal(root))
